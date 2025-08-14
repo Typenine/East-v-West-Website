5bf12e046aa5e682453b7d774f38dbd64f861041
Patrick McNulty <patrickmmcnulty62@yahoo.com>
2025-08-13 23:22:30 -0700

    Vercel config: standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  distDir: '.next',
+  /* other config options */
 };
 
 export default nextConfig;
